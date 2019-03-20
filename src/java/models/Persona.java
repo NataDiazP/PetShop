1d1c1ab51ece9202fa2e1b73ce260dd5a5903a33
Mateo Martínez <matmartinezpal@unal.edu.co>
2019-03-19 21:26:29 -0500

    Hermosura

---
+++ src/java/models/Persona.java
@@ -0,0 +1,178 @@
+package petshop;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class Persona {
+    
+    public static List<Persona> personas = new ArrayList<Persona>();
+    public static int contador_ids = 0;
+    
+    private int id;
+    private String nombre;
+    private String email;
+    private String telefono;
+    private String direccion;
+    private String password;
+    private List<Producto> lista_deseos;
+    private List<Comentario> lista_comentarios;
+    private List<Pedido> lista_pedidos;
+    
+    public Persona(){
+        
+        this.id = Persona.contador_ids + 1;
+        this.nombre = "";
+        this.email = "";
+        this.telefono = "";
+        this.direccion = "";
+        this.password = "";
+        this.lista_deseos = new ArrayList<Producto>();
+        this.lista_comentarios = new ArrayList<Comentario>();
+        this.lista_pedidos = new ArrayList<Pedido>();
+          
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Producto> getLista_deseos() {
+        return lista_deseos;
+    }
+
+    public void setLista_deseos(List<Producto> lista_deseos) {
+        this.lista_deseos = lista_deseos;
+    }
+
+    public List<Comentario> getLista_comentarios() {
+        return lista_comentarios;
+    }
+
+    public void setLista_comentarios(List<Comentario> lista_comentarios) {
+        this.lista_comentarios = lista_comentarios;
+    }
+
+    public List<Pedido> getLista_pedidos() {
+        return lista_pedidos;
+    }
+
+    public void setLista_pedidos(List<Pedido> lista_pedidos) {
+        this.lista_pedidos = lista_pedidos;
+    }
+    
+    
+    public Map<String, String> registrarse(String nombre, String email, String telefono, String direccion, String password, Map<String, String> mensajes){
+     
+        Map<String, String> retorno = new HashMap<String, String>();
+        
+        for (Persona persona_actual :  personas){
+            if (persona_actual.getEmail().equals(email)){
+                retorno.put("exitoso","False");
+                retorno.put("mensaje", mensajes.get("error_register"));
+                return retorno;
+            }
+        }
+        
+        setNombre(nombre);
+        setEmail(email);
+        setTelefono(telefono);
+        setDireccion(direccion);
+        setPassword(password);
+        
+        Persona.personas.add(this);
+        
+        retorno.put("exitoso","True");
+        retorno.put("mensaje", mensajes.get("succes_register"));
+        return retorno;
+           
+    }
+    
+    public Map<String, String> iniciar_sesion (Map<String, String> mensajes){
+        
+        Map<String, String> retorno = new HashMap<String, String>();
+        for (Persona persona_actual :  personas){
+            if (persona_actual.getEmail().equals(this.getEmail()) && persona_actual.getPassword().equals(this.getPassword())){
+                this.setId(persona_actual.getId());
+                this.setNombre(persona_actual.getNombre());
+                this.setTelefono(persona_actual.getTelefono());
+                this.setDireccion(persona_actual.getDireccion());
+                this.setLista_deseos(persona_actual.getLista_deseos());
+                this.setLista_comentarios(persona_actual.getLista_comentarios());
+                this.setLista_pedidos(persona_actual.getLista_pedidos());
+                
+                retorno.put("exitoso","True");
+                retorno.put("mensaje", mensajes.get("succes_login"));
+                return retorno;        
+            }        
+        }
+        retorno.put("exitoso","False");
+        retorno.put("mensaje", mensajes.get("error_login"));
+        return retorno;
+        
+    }
+    
+    public Map<String, String> agregar_lista_deseos(Producto producto,Map<String, String> mensajes){
+        
+        Map<String, String> retorno = new HashMap<String, String>();
+        for (Producto producto_actual: this.lista_deseos){
+            if (producto_actual.getId() == producto.getId()){
+                retorno.put("exitoso","False");
+                retorno.put("mensaje", mensajes.get("product_already_added"));
+                return retorno;
+            }
+        }
+        this.lista_deseos.add(producto);
+        
+        retorno.put("exitoso","True");
+        retorno.put("mensaje", mensajes.get("product_added"));
+        return retorno;
+    }
+       
+}
---
+++ src/java/models/Producto.java
@@ -0,0 +1,154 @@
+package petshop;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+public class Producto {
+    
+    public static List<Producto> productos = new ArrayList<Producto>();
+    public static int contador_ids = 0;
+    
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private float valor;
+    private int cantidad_inventario;
+    private List<PedidosProducto> lista_pedidos_producto;
+    private List<Comentario> lista_comentarios;
+    
+    public Producto(){
+        
+        this.id = Producto.contador_ids + 1;
+        this.nombre = "";
+        this.descripcion = "";
+        this.valor = 0;
+        this.cantidad_inventario = 0;
+        this.lista_pedidos_producto = new ArrayList<PedidosProducto>();
+        this.lista_comentarios = new ArrayList<Comentario>();
+      
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public int getCantidad_inventario() {
+        return cantidad_inventario;
+    }
+
+    public void setCantidad_inventario(int cantidad_inventario) {
+        this.cantidad_inventario = cantidad_inventario;
+    }
+
+    public List<PedidosProducto> getLista_pedidos_producto() {
+        return lista_pedidos_producto;
+    }
+
+    public void setLista_pedidos_producto(List<PedidosProducto> lista_pedidos_producto) {
+        this.lista_pedidos_producto = lista_pedidos_producto;
+    }
+
+    public List<Comentario> getLista_comentarios() {
+        return lista_comentarios;
+    }
+
+    public void setLista_comentarios(List<Comentario> lista_comentarios) {
+        this.lista_comentarios = lista_comentarios;
+    }
+    
+    public String validarExistenciaEnLista (Map<String, String> mensajes){
+        for (Producto producto_actual : Producto.productos){
+            if (producto_actual.getNombre().toLowerCase().equals(this.getNombre().toLowerCase())){
+                return mensajes.get("product_with_same_name");
+            }
+        }
+        
+        Producto.productos.add(this);
+        return mensajes.get("product_added");
+    }
+    
+    public void actualizarProducto(String nombre, float valor, String descripcion, int cantidad_inventario){
+        this.nombre = nombre;
+        this.valor = valor;
+        this.descripcion = descripcion;
+        this.cantidad_inventario = cantidad_inventario;
+    }
+    
+    public boolean validarCantidadInventario(int cantidad_venta){
+        return (this.cantidad_inventario >= cantidad_venta);
+    }
+    
+    public static List<Producto> buscarProductoNombre(String nombre_a_buscar){
+        List<Producto> listado_productos_buscados = new ArrayList<Producto>();
+        
+        for (Producto producto_actual : Producto.productos){
+            if (producto_actual.getNombre().toLowerCase().contains(nombre_a_buscar.toLowerCase())){
+                listado_productos_buscados.add(producto_actual);
+            }
+            
+        }
+        
+        return listado_productos_buscados;
+    }
+    
+    public static Producto seleccionarProducto(int numero_id){
+        for(Producto producto_actual : Producto.productos){
+            if (producto_actual.getId() == numero_id){
+                return producto_actual;
+            }
+        }
+        
+        return null;
+        
+    }
+    
+    public static String borrarProducto (int numero_id, Map<String, String> mensajes){
+        for (Producto producto_actual : Producto.productos){
+            if (producto_actual.getId() == numero_id){
+                Producto.productos.remove(producto_actual);
+                return mensajes.get("product_deleted");
+            }
+        }
+        return mensajes.get("product_not_found");
+    }
+    
+    public static boolean validarIdEnListaProductosAComentar (int numero_id, List<Producto> lista_productos){
+        for(Producto producto_actual : lista_productos){
+            if(numero_id == producto_actual.getId()){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+}
